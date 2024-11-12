4a228e49a5065b6d7a716466f35a17e3ba87f431
bayzedahmedandthe <bayzedm784@.com>
2024-11-12 14:38:20 +0600

    added responsive navbar and route setup

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home.jsx';
+import Layout from './Layout/Layout.jsx';
+import Login from './Components/Login/Login.jsx';
+import SignUp from './Components/SignUp/SignUp.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path:"/",
+        element: <Home></Home>
+      },
+      {
+        path: "login",
+        element: <Login></Login>
+      },
+      {
+        path: "signup",
+        element: <SignUp></SignUp>
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
